cbe9cec792f88ebec0bcf9f12b5eea11bb898999
manjuchavva135 <52740830+manjuchavva135@users.noreply.github.com>
2020-09-13 11:32:56 +0530

    c

--- routes/livestream.js
+++ routes/livestream.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {addstreamdetails, getallstreams,getbyname } = require('../controllers/livestream');
-
+const { requireSignin, isAuth } = require('../controllers/auth');
 
 
 
 router.post('/livestream',addstreamdetails);
 
-router.get('/livestream/find',  requireSignin, isAuth,getallstreams);
+router.get('/livestream/find', requireSignin, isAuth,getallstreams);
 router.get('/livestream/findname', getbyname);
 
 
